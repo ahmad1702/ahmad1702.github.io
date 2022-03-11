9776f1c91a4e3d43c169168afb32697967e339b8
ahmad1702 <ahmad1702@gmail.com>
2022-03-10 22:10:29 -0600

    development page integration

---
+++ development/dev.js
@@ -0,0 +1,23 @@
+
+
+
+let projImg = document.querySelector(".img-container");
+
+
+// projImg.addEventListener("mouseover", function(event){
+//     const currImg = event.target;
+//     currImg.children[0].style.display = "block";
+//     // currImg.style.backdropfilter = "brightness(5)"
+// });
+
+// projImg.addEventListener("mouseleave", function(event){
+//     const currImg = event.target;
+    
+//     currImg.children[0].style.display = "none";
+// });
+
+document.getElementById("img-1").addEventListener("click", function(){
+     window.open('https://github.com/ahmad1702/Delta-Tablet-SignUp-Python', '_blank');
+});
+
+
