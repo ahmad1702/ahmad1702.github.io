3e9b9e96f6ac4a6fe6406504c39f0f9cf4a18539
Ahmad Sandid <ahmad1702@gmail.com>
2022-04-22 10:11:20 -0500

    changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,45 @@
-import React from "react";
-import Skills from '../Skills/Skills';
-import Nav from '../Nav/Nav';
-import './Home.scss';
+import React, { useEffect } from "react";
+import Skills from "../Skills/Skills";
+import Nav from "../Nav/Nav";
+import "./Home.scss";
+
+// Animation
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 export default function Home() {
+  const controls = useAnimation();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("show");
+      console.log("inView");
+    }
+    if (!inView) {
+      controls.start("hidden");
+      console.log("not inView");
+    }
+  }, [controls, inView]);
+
+  const container = {
+    hidden: { x: -50, opacity: 0 },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        ease: [0.6, 0.01, -0.05, 0.95],
+        staggerChildren: 0.5,
+        duration: 1,
+      },
+    },
+  };
+
   return (
     <div className="Home">
       <div className="header-container">
         <Nav />
-        <header>
+        <motion.header variants={container} initial="hidden" animate="show">
           <h1 className="header-title">Ahmad Sandid</h1>
           <p className="job-title">Web Developer</p>
           <p className="blurb">
@@ -16,9 +47,9 @@ export default function Home() {
             or data mining using Machine Learning, I find that being an adaptive
             problem-solver will allow you to excel in about almost any field.
           </p>
-        </header>
+        </motion.header>
       </div>
-      <Skills />
+      <Skills ref={ref} variants={container} initial="hidden" animate={controls} />
     </div>
   );
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,10 +6,42 @@ import Dev from "../Dev/Dev";
 import VisualArt from "../VisualArt/Visualart";
 import ErrorPage from "../Error/ErrorPage";
 
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 export default function Nav() {
+  const [titleRef, titleInView] = useInView({
+    triggerOnce: true,
+    rootMargin: "-100px 0px",
+  });
+  const container = {
+    hidden: { x: -50, opacity: 0 },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        ease: [0.6, 0.01, -0.05, 0.95],
+        staggerChildren: 0.5,
+        duration: 1,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0, x: -50 },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        ease: [0.6, 0.01, -0.05, 0.95],
+        duration: 1.6,
+      },
+    },
+  };
   return (
     <div>
-      <nav className="main-nav">
+      <motion.nav variants={container} className="main-nav" initial="hidden" animate="show">
         <div className="logo">
           <Link to="/">
             <h1 id="water">AHMAD</h1>
@@ -32,7 +64,7 @@ export default function Nav() {
             <li>Design</li>
           </Link> */}
         </div>
-      </nav>
+      </motion.nav>
       <nav className="mobilenavigation">
         <div id="menuToggle" className="animated animatedFadeInUp fadeInUp">
           <input type="checkbox" id="mobilenavcheck"/>
